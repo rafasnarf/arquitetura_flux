70066bd154d92e7f11a72120b5df6432d346bbd4
rafasnarf <snarfelcv@gmail.com>
2020-02-06 23:15:43 -0300

    First Commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
+
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/02/HZM-1731-002/HZM-1731-002_zoom1.jpg"
+          alt="Tênis charmoso"
+        />
+        <strong>Tênis muito Garboso</strong>
+        <span>R$129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/02/HZM-1731-002/HZM-1731-002_zoom1.jpg"
+          alt="Tênis charmoso"
+        />
+        <strong>Tênis muito Garboso</strong>
+        <span>R$129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/02/HZM-1731-002/HZM-1731-002_zoom1.jpg"
+          alt="Tênis charmoso"
+        />
+        <strong>Tênis muito Garboso</strong>
+        <span>R$129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/02/HZM-1731-002/HZM-1731-002_zoom1.jpg"
+          alt="Tênis charmoso"
+        />
+        <strong>Tênis muito Garboso</strong>
+        <span>R$129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/02/HZM-1731-002/HZM-1731-002_zoom1.jpg"
+          alt="Tênis charmoso"
+        />
+        <strong>Tênis muito Garboso</strong>
+        <span>R$129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/02/HZM-1731-002/HZM-1731-002_zoom1.jpg"
+          alt="Tênis charmoso"
+        />
+        <strong>Tênis muito Garboso</strong>
+        <span>R$129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
+}
